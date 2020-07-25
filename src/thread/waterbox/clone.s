6446985d83d5810ee84bab5e5c107226a22572c7
nattthebear <goyuken@gmail.com>
2020-07-25 15:38:05 -0400

    Simplify NR_WBX_CLONE: Implementation details of how waterbox host first enters the child thread are now all handled within the host.  The guest simply passes child_rip as an argument.

--- src/thread/waterbox/clone.s
+++ src/thread/waterbox/clone.s
@@ -2,25 +2,22 @@
 .global __clone
 .hidden __clone
 .type   __clone,@function
-__clone:
+__clone: /* (entry_point, stack, flags, arg, ptid, tls, ctid) */
 	/* syscall number - NR_WBX_CLONE */
 	xor %eax,%eax
 	mov $2000,%ax
 
 	/* set up information on child stack */
 	and $-16,%rsi /* align */
-	sub $24,%rsi
-	mov %rdi,16(%rsi) /* thread entry point */
-	mov %rcx,8(%rsi) /* thread entry argument */
-	lea child_thread_start(%rip),%r10
-	mov %r10,(%rsi) /* host will ret in child thread... */
-	sub $8,%rsi /* ... after popping 1 regs */
+	sub $16,%rsi
+	mov %rdi,8(%rsi) /* thread entry point */
+	mov %rcx,0(%rsi) /* thread entry argument */
 
 	mov %r9,%rdi /* tls */
-	mov %r8,%rdx /* parent_tid */
 	mov 8(%rsp),%rcx /* child_tid */
+	lea child_thread_start(%rip),%rdx /* child_rip */
 
-	/* syscall NR_WBX_CLONE (tls, child_rsp, parent_tid, child_tid) */
+	/* syscall NR_WBX_CLONE (tls, child_rsp, child_rip, child_tid, parent_tid) */
 	mov $0x35f00000080,%r10
 	call *%r10
 	ret
