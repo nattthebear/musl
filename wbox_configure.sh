9ab8fd6f7908e64d9dcd335ef24c4fe08b3971a2
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-01-17 01:27:47 -0800

    changes for building waterbox with clang / llvm tools instead of gcc and co

--- wbox_configure.sh
+++ wbox_configure.sh
@@ -2,8 +2,9 @@
 set -e
 if [ -z "$SYSROOT" ]; then export SYSROOT="$(realpath "$(dirname "$0")/../sysroot")"; fi
 
-export LDFLAGS=""
-export CFLAGS="-Werror -fvisibility=hidden -mcmodel=large -mstack-protector-guard=global -no-pie -fno-pic -fno-pie" # TODO: Add -flto
-export AR="gcc-ar"
-export RANLIB="gcc-ranlib"
+export CC="clang"
+export LDFLAGS="-Wl,-no-pie -fuse-ld=lld"
+export CFLAGS="-Werror -fvisibility=hidden -mcmodel=large -mstack-protector-guard=global -fno-pic -fno-pie" # TODO: Add -flto
+export AR="llvm-ar"
+export RANLIB="llvm-ranlib"
 ./configure --target=waterbox --build=waterbox --disable-shared --prefix="$SYSROOT" --syslibdir="$SYSROOT/syslib"
