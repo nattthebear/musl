dcecb055f760d49b312936bc24db31882ee7abf1
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-03-22 00:44:50 -0700

    Remove AR/RANLIB specifications
    These don't really matter in any case (they just archive .o fies, how can you screw that up?) configure will figure out whatever ar/ranlib is available and use that

--- wbox_configure.sh
+++ wbox_configure.sh
@@ -4,6 +4,4 @@ if [ -z "$SYSROOT" ]; then export SYSROOT="$(realpath "$(dirname "$0")/../sysroo
 if [ -z "$CC" ]; then export CC="clang"; fi
 
 export CFLAGS="-Werror -fvisibility=hidden -mcmodel=large -mstack-protector-guard=global -fno-pic -fno-pie -fcf-protection=none" # TODO: Add -flto
-export AR="llvm-ar"
-export RANLIB="llvm-ranlib"
 ./configure --target=waterbox --build=waterbox --disable-shared --prefix="$SYSROOT" --syslibdir="$SYSROOT/syslib"
