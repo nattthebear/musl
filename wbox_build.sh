e7aec3b9ab56cd86aff9838f56c154f1f73080f1
CasualPokePlayer <50538166+CasualPokePlayer@users.noreply.github.com>
2023-03-28 16:33:39 -0700

    Update setup for clang (#4)

--- wbox_build.sh
+++ wbox_build.sh
@@ -3,10 +3,5 @@ set -e
 if [ -z "$SYSROOT" ]; then export SYSROOT="$(realpath "$(dirname "$0")/../sysroot")"; fi
 
 mkdir -p "$SYSROOT"
-make
+make -j
 make install
-# To uselibclang-rt (and libunwind), we need to evict the gcc libs, but musl automatically puts that in the specs file and has no setting to remove it. Fix that now by clearing the 13th line.
-cd "$SYSROOT/lib"
-cp "musl-gcc.specs" "musl-gcc.broken.specs"
-awk 'NR==13{print""};NR!=13' "musl-gcc.broken.specs" >"musl-gcc.specs" # clears the line after "*libgcc:"
-rm "musl-gcc.broken.specs"
--- wbox_configure.sh
+++ wbox_configure.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 set -e
 if [ -z "$SYSROOT" ]; then export SYSROOT="$(realpath "$(dirname "$0")/../sysroot")"; fi
+if [ -z "$CC" ]; then export CC="clang"; fi
 
-export LDFLAGS=""
-export CFLAGS="-Werror -fvisibility=hidden -mcmodel=large -mstack-protector-guard=global -no-pie -fno-pic -fno-pie" # TODO: Add -flto
-export AR="gcc-ar"
-export RANLIB="gcc-ranlib"
+export CFLAGS="-Werror -fvisibility=hidden -mcmodel=large -mstack-protector-guard=global -fno-pic -fno-pie -fcf-protection=none" # TODO: Add -flto
 ./configure --target=waterbox --build=waterbox --disable-shared --prefix="$SYSROOT" --syslibdir="$SYSROOT/syslib"
